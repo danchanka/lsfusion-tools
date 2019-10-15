0718afb677168a5a11137461db551cceeb819719
danchanka <danchenko@gmail.com>
2019-10-15 17:45:27 +0300

    Bug fix in documentation translation tools

--- confluence/ConvertForTranslation/convert.py
+++ confluence/ConvertForTranslation/convert.py
@@ -9,30 +9,29 @@ def convert(dir, outdir):
 		with open(dir + '/' + filename, 'r', encoding='utf-8') as infile:
 		    data = infile.read()
 
-		data = re.sub(r'<ac:link><ri:page ri:content-title="(.*?)" /></ac:link>', 
-			r'<a href="\1">\1</a>', data)
-
-		data = re.sub(r'<ac:link><ri:page ri:space-key="LSFUS" ri:content-title="(.*?)" /></ac:link>', 
-			r'<a href="\1__LSFUS">\1</a>', data)
-
-		data = re.sub(r'<ac:link><ri:page ri:content-title="(.*?)" /><ac:plain-text-link-body><!\[CDATA\[(.*?)\]\]></ac:plain-text-link-body></ac:link>', 
+		data = re.sub(r'<ac:link><ri:page ri:content-title="([^"]*?)" /><ac:plain-text-link-body><!\[CDATA\[(.*?)\]\]></ac:plain-text-link-body></ac:link>', 
 			r'<a href="\1__DATA">\2</a>', data)
 
-		data = re.sub(r'<ac:link><ri:page ri:space-key="LSFUS" ri:content-title="(.*?)" /><ac:plain-text-link-body><!\[CDATA\[(.*?)\]\]></ac:plain-text-link-body></ac:link>', 
+		data = re.sub(r'<ac:link><ri:page ri:space-key="LSFUS" ri:content-title="([^"]*?)" /><ac:plain-text-link-body><!\[CDATA\[(.*?)\]\]></ac:plain-text-link-body></ac:link>', 
 			r'<a href="\1__LSFUS__DATA">\2</a>', data)
 
-		data = re.sub(r'<ac:link><ri:page ri:content-title="(.*?)" /><ac:link-body>(.*?)</ac:link-body></ac:link>', 
+		data = re.sub(r'<ac:link><ri:page ri:content-title="([^"]*?)" /><ac:link-body>(.*?)</ac:link-body></ac:link>', 
 			r'<a href="\1__BODY">\2</a>', data)
 
+		data = re.sub(r'<ac:link><ri:page ri:content-title="([^"]*?)" /></ac:link>', 
+			r'<a href="\1">\1</a>', data)
+
+		data = re.sub(r'<ac:link><ri:page ri:space-key="LSFUS" ri:content-title="([^"]*?)" /></ac:link>', 
+			r'<a href="\1__LSFUS">\1</a>', data)
 
 
-		data = re.sub(r'<ac:link ac:anchor="(.*?)"><ac:plain-text-link-body><!\[CDATA\[(.*?)\]\]></ac:plain-text-link-body></ac:link>', 
-			r'<a href="#\1__DATA">\2</a>', data)
-
-		data = re.sub(r'<ac:link ac:anchor="(.*?)"><ri:page ri:content-title="(.*?)" /><ac:plain-text-link-body><!\[CDATA\[(.*?)\]\]></ac:plain-text-link-body></ac:link>', 
+		data = re.sub(r'<ac:link ac:anchor="([^"]*?)"><ri:page ri:content-title="([^"]*?)" /><ac:plain-text-link-body><!\[CDATA\[(.*?)\]\]></ac:plain-text-link-body></ac:link>', 
 			r'<a href="\2#\1__DATA">\3</a>', data)
 
-		data = re.sub(r'<ac:link ac:anchor="(.*?)"><ri:page ri:content-title="(.*?)" /><ac:link-body>(.*?)</ac:link-body></ac:link>', 
+		data = re.sub(r'<ac:link ac:anchor="([^"]*?)"><ac:plain-text-link-body><!\[CDATA\[(.*?)\]\]></ac:plain-text-link-body></ac:link>', 
+			r'<a href="#\1__DATA">\2</a>', data)
+
+		data = re.sub(r'<ac:link ac:anchor="([^"]*?)"><ri:page ri:content-title="([^"]*?)" /><ac:link-body>(.*?)</ac:link-body></ac:link>', 
 			r'<a href="\2#\1__BODY">\3</a>', data)
 
 		with open(outdir + '/' + filename, 'w', encoding='utf-8') as outfile:
--- confluence/ConvertForTranslation/restore.py
+++ confluence/ConvertForTranslation/restore.py
@@ -10,28 +10,28 @@ def restore(dir, outdir):
 		    data = infile.read()
 
 
-		    data = re.sub(r'<a href="(?!http)(.+?)#(.*?)__BODY">(.*?)</a>', 
+		    data = re.sub(r'<a href="(?!http)([^"]+?)#([^"]*?)__BODY">(.*?)</a>', 
 		    	r'<ac:link ac:anchor="\2"><ri:page ri:content-title="\1" /><ac:link-body>\3</ac:link-body></ac:link>', data) 
 
-		    data = re.sub(r'<a href="(?!http)(.+?)#(.*?)__DATA">(.*?)</a>', 
+		    data = re.sub(r'<a href="(?!http)([^"]+?)#([^"]*?)__DATA">(.*?)</a>', 
 		    	r'<ac:link ac:anchor="\2"><ri:page ri:content-title="\1" /><ac:plain-text-link-body><![CDATA[\3]]></ac:plain-text-link-body></ac:link>', data) 
 
-		    data = re.sub(r'<a href="#(.*?)__DATA">(.*?)</a>', 
+		    data = re.sub(r'<a href="#([^"]*?)__DATA">(.*?)</a>', 
 		    	r'<ac:link ac:anchor="\1"><ac:plain-text-link-body><![CDATA[\2]]></ac:plain-text-link-body></ac:link>', data) 
 
-		    data = re.sub(r'<a href="(?!http)(.+?)__BODY">(.*?)</a>', 
+		    data = re.sub(r'<a href="(?!http)([^"]+?)__BODY">(.*?)</a>', 
 		    	r'<ac:link><ri:page ri:content-title="\1" /><ac:link-body>\2</ac:link-body></ac:link>', data)	
 
-		    data = re.sub(r'<a href="(?!http)(.+?)__LSFUS_DATA">(.*?)</a>', 
+		    data = re.sub(r'<a href="(?!http)([^"]+?)__LSFUS__DATA">(.*?)</a>', 
 		    	r'<ac:link><ri:page ri:space-key="LSFUS" ri:content-title="\1" /><ac:plain-text-link-body><![CDATA[\2]]></ac:plain-text-link-body></ac:link>', data)	
 
-		    data = re.sub(r'<a href="(?!http)(.+?)__DATA">(.*?)</a>', 
+		    data = re.sub(r'<a href="(?!http)([^"]+?)__DATA">(.*?)</a>', 
 		    	r'<ac:link><ri:page ri:content-title="\1" /><ac:plain-text-link-body><![CDATA[\2]]></ac:plain-text-link-body></ac:link>', data)	
 
-		    data = re.sub(r'<a href="(?!http)(.+?)__LSFUS">(.*?)</a>', 
+		    data = re.sub(r'<a href="(?!http)([^"]+?)__LSFUS">(.*?)</a>', 
 		    	r'<ac:link><ri:page ri:space-key="LSFUS" ri:content-title="\1" /></ac:link>', data)	
 
-		    data = re.sub(r'<a href="(?!http)(.+?)">(.*?)</a>', 
+		    data = re.sub(r'<a href="(?!http)([^"]+?)">(.*?)</a>', 
 		    	r'<ac:link><ri:page ri:content-title="\1" /></ac:link>', data)	
 
 		with open(outdir + '/' + filename, 'w', encoding='utf-8') as outfile:
